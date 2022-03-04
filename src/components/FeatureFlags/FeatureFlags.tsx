8cbfae38d5e4dd1231329d5f575b24125fe3a5ab
Johan Friis <johanfriis@gmail.com>
2022-03-04 13:42:57 +0100

    Avoid rerendering all blocks on flag toggle

--- src/components/FeatureFlags/FeatureFlags.tsx
+++ src/components/FeatureFlags/FeatureFlags.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { FeatureFlagsProvider } from './context/featureFlagsContext';
 import { FeatureFlagSection } from './components/FeatureFlagSection/FeatureFlagSection';
 import { schema } from './schema'
 
 import './FeatureFlags.css';
 
 export const FeatureFlags = () => {
-  return <FeatureFlagsProvider>
-    <div>
+  return <div>
       <div className="general-flags">
         <FeatureFlagSection {...schema.general} />
       </div>
@@ -17,8 +15,4 @@ export const FeatureFlags = () => {
         <FeatureFlagSection {...schema.alerts} />
       </div>
     </div>
-    {/* {schema.map(section => (
-      <FeatureFlagSection {...section} key={section.id} />
-    ))} */}
-  </FeatureFlagsProvider>
 }
--- src/components/FeatureFlags/components/FeatureFlagSection/FeatureFlagSection.tsx
+++ src/components/FeatureFlags/components/FeatureFlagSection/FeatureFlagSection.tsx
@@ -1,22 +1,35 @@
-import React from "react"
-import { Section } from "../../types"
-import { FeatureFlagBlock } from "../FeatureFlagBlock/FeatureFlagBlock"
+import React from 'react'
+import { Section } from '../../types'
+import { FeatureFlagBlock } from '../FeatureFlagBlock/FeatureFlagBlock'
+import { FeatureFlagsProvider } from '../../context/featureFlagsContext'
 
-import "./FeatureFlagSection.css"
+import './FeatureFlagSection.css'
 
 type Props = Section
 
-export const FeatureFlagSection = ({id, label, gap, fillBackground, width, wrap, blocks}: Props) => {
-  return <div className="feature-flag-section">
-    <h2>{label}</h2>
-    <div className={`
+export const FeatureFlagSection = ({
+  id,
+  label,
+  gap,
+  fillBackground,
+  blocks,
+}: Props) => {
+  return (
+    <div className="feature-flag-section">
+      <h2>{label}</h2>
+      <div
+        className={`
       feature-flag-section__blocks
       ${gap ? 'feature-flag-section__blocks--gap' : ''}
       ${fillBackground ? 'feature-flag-section--fill-background' : ''}
-    `}>
-      {blocks.map((block, idx) =>
-        <FeatureFlagBlock block={block} sectionId={id} key={idx} />
-      )}
+    `}
+      >
+        {blocks.map((block, idx) => (
+          <FeatureFlagsProvider key={idx}>
+            <FeatureFlagBlock block={block} sectionId={id} />
+          </FeatureFlagsProvider>
+        ))}
+      </div>
     </div>
-  </div>
+  )
 }
